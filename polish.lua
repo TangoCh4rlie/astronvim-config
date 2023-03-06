4dce30f64cb03b1b4fb92ba61017f0341596bcb4
Neylix <julien.leclerc05@protonmail.com>
2023-03-06 10:15:41 +0100

    Add auto resize

--- polish.lua
+++ polish.lua
@@ -2,4 +2,7 @@ return function()
   -- Workaround for indent_blankline color
   local whitespace_color = vim.fn.synIDattr(vim.fn.hlID('Whitespace'), 'fg')
   vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = whitespace_color, nocombine = true })
+
+  -- Auto resize split when window os resized
+  vim.api.nvim_create_autocmd('VimResized', { command = 'wincmd =' })
 end
