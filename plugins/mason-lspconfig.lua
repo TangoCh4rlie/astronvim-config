c1a78f4a981c9ca40b9c91ba26db79cc8c29f515
TangoCh4rlie <elouanreymond@tutanota.com>
2023-10-04 10:50:23 +0200

    Ajout de LSP

--- plugins/mason-lspconfig.lua
+++ plugins/mason-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = { "lua_ls", "elixirls", "pylsp" }
+    ensure_installed = { "lua_ls", "elixirls", "pylsp", "clangd", "bashls", "cssls", "html", "jsonls", "jdtls", "quick_lint_js", "marksman", "intelephense"}
   }
 }
