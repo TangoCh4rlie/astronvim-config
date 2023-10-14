5e4b5b935c4b0f2588a547fbd616e37887d00dd1
TangoCh4rlie <elouanreymond@tutanota.com>
2023-10-14 15:01:59 +0200

    fix indent-blankline

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -9,8 +9,8 @@ return {
 	 hide_gitignored = true,
 	 hide_by_name = {
 	   -- '.git',
-	   -- '.DS_Store',
-	   -- 'thumbs.db',
+	   '.DS_Store',
+	   'thumbs.db',
 	 },
 	never_show = {},
       },
--- polish.lua
+++ polish.lua
@@ -1,7 +1,4 @@
 return function()
-  -- Workaround for indent_blankline color
-  local whitespace_color = vim.fn.synIDattr(vim.fn.hlID('Whitespace'), 'fg')
-  vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = whitespace_color, nocombine = true })
 
   -- Auto resize split when window os resized
   vim.api.nvim_create_autocmd('VimResized', { command = 'wincmd =' })
